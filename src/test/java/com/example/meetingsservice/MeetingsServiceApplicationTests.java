b486aa505617ff87e43654456903c6428cb21d36
Jullliya <vamoyu2@gmail.com>
2023-04-12 14:01:20 +0300

    Init commit

---
+++ src/test/java/com/example/meetingsservice/MeetingsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meetingsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
